1f82de7f4a3c359f27043edb4f8450b02e07bb16
hanguyen291298 <thuyha291298@gmail.com>
2023-09-11 22:41:10 +0700

    Create effection for buttons to listen when it's clicked

--- index.js
+++ index.js
@@ -29,30 +29,58 @@ console.log(Operators["+"])
 
 
 const buttons = document.querySelectorAll("button");
+
 let enter_data = document.querySelector(".enter");
 let first_value = document.querySelector(".first_value");
 let second_value = document.querySelector(".second_value")
 let operator = document.querySelector(".operator");
 let result;
-
-let current_content = "";
+let current_number = "";
 
 for (const bnt of buttons){
     
     bnt.addEventListener("click", ()=>{
-
-        const value = bnt.textContent
         
+        let get_value = bnt.textContent
+
         if (bnt.classList.contains("num")){
-            enter_data.innerHTML = current_content + value
-            current_content = enter_data.textContent
+            enter_data.innerHTML = current_number + get_value
+            current_number += get_value
         }
-        else{
-            current_content = "";
-            first_value.innerHTML = enter_data.textContent;
-            operator.innerHTML = value;
 
+        else if (bnt.classList.contains("equal")) { 
 
+            result = Operators[operator.textContent](first_value.textContent, enter_data.textContent)
+            second_value.innerHTML = enter_data.textContent
+            enter_data.innerHTML = result
+            
+        }
+
+        else if (bnt.classList.contains("AC")) {
+            
+            first_value.innerHTML = "";
+            second_value.innerHTML = "";
+            operator.innerHTML = "";
+            enter_data.innerHTML = "0";
+            current_number = "";
+        }
+        else if (bnt.classList.contains("CE")){
+
+            if (current_number.length > 1){
+                current_number = current_number.slice(0, current_number.length -1)
+                enter_data.innerHTML = current_number
+            }
+            else {
+                enter_data.innerHTML = "0"
+            }
+        }   
+        
+        else {
+
+            first_value.innerHTML = enter_data.textContent;         
+            operator.innerHTML = get_value
+            second_value.innerHTML = "";
+            current_number = "";
         }
 
     })
@@ -62,19 +90,8 @@ for (const bnt of buttons){
 
 // create a funtion to call equal funtion to return a result
 
-const equal = document.querySelector(".equal")
 
 
-equal.addEventListener("click", (f)=>{
-    let func = Operators[operator.textContent]
-    console.log(func)
-    
-    second_value = enter_data.textContent
-    console.log(first_value.textContent)
-    console.log(second_value.textContent)
-    result = func(first_value.textContent, second_value.textContent)
-    enter_data.innerHTML = result
-})
 
 
 
